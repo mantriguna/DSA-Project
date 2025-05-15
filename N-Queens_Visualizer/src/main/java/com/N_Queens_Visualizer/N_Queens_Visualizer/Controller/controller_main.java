049a001b5e58504cdb71b141626d5b290a8cdea1
mantriguna <mantriguna0106@gmail.com>
2025-05-15 23:02:14 +0530

    setup files

---
+++ N-Queens_Visualizer/src/main/java/com/N_Queens_Visualizer/N_Queens_Visualizer/Controller/controller_main.java
@@ -0,0 +1,5 @@
+package com.N_Queens_Visualizer.N_Queens_Visualizer.Controller;
+
+public class controller_main {
+
+}
---
+++ N-Queens_Visualizer/src/main/java/com/N_Queens_Visualizer/N_Queens_Visualizer/Service/Service_main.java
@@ -0,0 +1,5 @@
+package com.N_Queens_Visualizer.N_Queens_Visualizer.Service;
+
+public class Service_main {
+
+}
