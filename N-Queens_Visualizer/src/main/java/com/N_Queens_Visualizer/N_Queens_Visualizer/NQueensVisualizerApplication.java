aefb7c1a679b7fe57c1fa326ae3ad535d2610c69
mantriguna <mantriguna0106@gmail.com>
2025-05-15 22:33:04 +0530

    let's start

---
+++ N-Queens_Visualizer/src/main/java/com/N_Queens_Visualizer/N_Queens_Visualizer/NQueensVisualizerApplication.java
@@ -0,0 +1,13 @@
+package com.N_Queens_Visualizer.N_Queens_Visualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NQueensVisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NQueensVisualizerApplication.class, args);
+	}
+
+}
---
+++ N-Queens_Visualizer/src/test/java/com/N_Queens_Visualizer/N_Queens_Visualizer/NQueensVisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.N_Queens_Visualizer.N_Queens_Visualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NQueensVisualizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
